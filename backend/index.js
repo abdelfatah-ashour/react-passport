ebdeef47105d2f47e74f61601b3ef7e98ee55cfe
abdelfatahashour4 <abdelfatahashour4@gmail.com>
2021-03-10 17:59:32 +0200

    add path to cors

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ app.use(
     origin: process.env.CLIENT_BASEURL, // allow to server to accept request from different origin
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
     credentials: true, // allow session cookie from browser to pass through
+    path: "/",
   }),
 );
 
