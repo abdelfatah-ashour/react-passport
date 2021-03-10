f491f7720ee9ebba31014fab02f4347fb56e646b
abdelfatahashour4 <abdelfatahashour4@gmail.com>
2021-03-10 18:10:43 +0200

    add backend url in a anchor

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,103 +8,106 @@ import { FaGoogle, FaFacebookF, FaGithubSquare, FaHome } from "react-icons/fa";
 import "../assets/form.css";
 
 export default function Login() {
-	const [user, setUser] = useState({
-		email: "",
-		password: "",
-	});
-	const { email, password } = user;
-	const handleChange = (e) => {
-		setUser({ ...user, [e.target.name]: e.target.value });
-	};
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = user;
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		await HTTP.post("/api/auth/login", user)
-			.then(({ data }) => {
-				toast.success(data.message);
-			})
-			.catch(({ response }) => {
-				toast.error(response.data.message);
-			});
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await HTTP.post("/api/auth/login", user)
+      .then(({ data }) => {
+        toast.success(data.message);
+      })
+      .catch(({ response }) => {
+        toast.error(response.data.message);
+      });
+  };
 
-	return (
-		<div className="login">
-			<div className="root-to-home">
-				<Link to="/">
-					<FaHome />
-				</Link>
-			</div>
-			<div className="container-form">
-				<div className="page-title">LOGIN</div>
-				<MainForm>
-					{/* email input */}
-					<InputForm
-						label="Email Address"
-						name="email"
-						value={email}
-						type="email"
-						placeholder="type email Address"
-						handleChange={handleChange}
-					/>
-					{/* password input */}
-					<InputForm
-						label="Password"
-						name="password"
-						value={password}
-						type="password"
-						placeholder="type password"
-						handleChange={handleChange}
-					/>
-					<div className="group-submit">
-						<button className="btn btn-success" onClick={handleSubmit}>
-							submit
-						</button>
-						<span>
-							don't have Account ?{"  "}
-							<Link to="/register">Register</Link>
-						</span>
-					</div>
-				</MainForm>
-				<div className="break-line">
-					<span>or</span>
-				</div>
-				<div className="container-social-media">
-					<a
-						className="login-social"
-						href="http://localhost:9000/auth/google"
-						style={{ backgroundColor: "rgb(232 34 34)" }}>
-						<span className="icon">
-							<FaGoogle />
-						</span>
-						<span className="main-social">
-							continue with <b>Google</b>
-						</span>
-					</a>
-					<a
-						className="login-social"
-						href="http://localhost:9000/auth/facebook"
-						style={{ backgroundColor: "rgb(56 56 144)" }}>
-						<span className="icon">
-							<FaFacebookF />
-						</span>
-						<span className="main-social">
-							continue with <b> Facebook</b>
-						</span>
-					</a>
-					<a
-						className="login-social"
-						href="http://localhost:9000/auth/github"
-						style={{ backgroundColor: "rgb(14 8 8)" }}>
-						<span className="icon">
-							<FaGithubSquare />
-						</span>
-						<span className="main-social">
-							continue with <b> Github</b>
-						</span>
-					</a>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="login">
+      <div className="root-to-home">
+        <Link to="/">
+          <FaHome />
+        </Link>
+      </div>
+      <div className="container-form">
+        <div className="page-title">LOGIN</div>
+        <MainForm>
+          {/* email input */}
+          <InputForm
+            label="Email Address"
+            name="email"
+            value={email}
+            type="email"
+            placeholder="type email Address"
+            handleChange={handleChange}
+          />
+          {/* password input */}
+          <InputForm
+            label="Password"
+            name="password"
+            value={password}
+            type="password"
+            placeholder="type password"
+            handleChange={handleChange}
+          />
+          <div className="group-submit">
+            <button className="btn btn-success" onClick={handleSubmit}>
+              submit
+            </button>
+            <span>
+              don't have Account ?{"  "}
+              <Link to="/register">Register</Link>
+            </span>
+          </div>
+        </MainForm>
+        <div className="break-line">
+          <span>or</span>
+        </div>
+        <div className="container-social-media">
+          <a
+            className="login-social"
+            href="https://react-passport.herokuapp.com/auth/google"
+            style={{ backgroundColor: "rgb(232 34 34)" }}
+          >
+            <span className="icon">
+              <FaGoogle />
+            </span>
+            <span className="main-social">
+              continue with <b>Google</b>
+            </span>
+          </a>
+          <a
+            className="login-social"
+            href="https://react-passport.herokuapp.com/auth/facebook"
+            style={{ backgroundColor: "rgb(56 56 144)" }}
+          >
+            <span className="icon">
+              <FaFacebookF />
+            </span>
+            <span className="main-social">
+              continue with <b> Facebook</b>
+            </span>
+          </a>
+          <a
+            className="login-social"
+            href="https://react-passport.herokuapp.com/auth/github"
+            style={{ backgroundColor: "rgb(14 8 8)" }}
+          >
+            <span className="icon">
+              <FaGithubSquare />
+            </span>
+            <span className="main-social">
+              continue with <b> Github</b>
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
